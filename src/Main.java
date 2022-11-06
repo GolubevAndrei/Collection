1c74f5ce9b8d36ebc968484bb01a6520187bfde8
GolubevAndrey <golubev_andrei87@mail.ru>
2022-11-06 11:03:19 +0300

    Задание первое выполненное

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,15 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
+        //Задание 1
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i % 2 == 0) {
+                System.out.println(nums.get(i));
+            }
+        }
+
 
     }
 }
